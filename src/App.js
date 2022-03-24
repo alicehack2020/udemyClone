c6a46373afa6986060e39399e59a997064c9baea
Mangesh2042 <mangesh2042@gmail.com>
2022-03-24 23:29:31 +0530

    issue fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/top/Header';
 import NavBar from './components/top/NavBar'; 
 import Hero from './components/main/Hero';
 import BroadSelectionCourse from './components/main/BroadSelectionCourse';
-import CourseSection from './components/main/CourseSection';
 function App() {
   return (
     <>
